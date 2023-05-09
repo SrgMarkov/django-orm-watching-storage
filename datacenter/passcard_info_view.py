a02697f9b45d36999cc8df55f8610af4da5ae917
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-05-09 09:24:40 +0400

    check gitignore

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,25 +1,22 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    try:
-        passcard = Passcard.objects.get(passcode=passcode)
-        this_passcard_visits = []
-        person_vizit = Visit.objects.filter(passcard=passcard)
-        for enter in range(len(person_vizit)):
-            pass_dict = {
-                'entered_at': person_vizit[enter].entered_at,
-                'duration': Visit.get_duration(person_vizit[enter]),
-                'is_strange': Visit.is_visit_long(person_vizit[enter])}
-            this_passcard_visits.append(pass_dict)
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    this_passcard_visits = []
+    employee_visits = Visit.objects.filter(passcard=passcard)
+    for visit in employee_visits:
+        pass_dict = {
+            'entered_at': visit.entered_at,
+            'duration': Visit.get_duration(visit),
+            'is_strange': Visit.is_visit_long(visit)}
+        this_passcard_visits.append(pass_dict)
 
-        context = {
-            'passcard': passcard,
-            'this_passcard_visits': this_passcard_visits
-        }
-        return render(request, 'passcard_info.html', context)
-    except Passcard.DoesNotExist:
-        raise Http404()
+    context = {
+        'passcard': passcard,
+        'this_passcard_visits': this_passcard_visits
+    }
+    return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,12 +5,12 @@ from django.utils import timezone
 
 def storage_information_view(request):
     non_closed_visits = []
-    entered = Visit.objects.filter(leaved_at=None)
-    for person in range(len(entered)):
-        person_dict = {'who_entered': entered[person].passcard,
-                       'entered_at': timezone.localtime(entered[person].entered_at),
-                       'duration': Visit.format_duration(entered[person]),
-                       'is_strange': Visit.is_visit_long(entered[person])}
+    active_visitors = Visit.objects.filter(leaved_at=None)
+    for visitor in active_visitors:
+        person_dict = {'who_entered': visitor.passcard,
+                       'entered_at': timezone.localtime(visitor.entered_at),
+                       'duration': Visit.format_duration(visitor),
+                       'is_strange': Visit.is_visit_long(visitor)}
         non_closed_visits.append(person_dict)
     context = {
         'non_closed_visits': non_closed_visits,
